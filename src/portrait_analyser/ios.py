413a141178d2924bfbe61b4073cdde704c2de39b
Michał Pasternak <michal.dtz@gmail.com>
2024-03-31 21:01:08 +0200

    Class for IOSImage, teeth analysis

--- src/portrait_analyser/ios.py
+++ src/portrait_analyser/ios.py
@@ -1,15 +1,60 @@
-from io import BytesIO
-from typing import List, Tuple
+from typing import Union
 
 import piexif
 import pyheif
-from PIL import Image
+from PIL import Image, ImageFilter, ImageOps
+from pyheif import UndecodedHeifImage
 
 from . import const
 from .exceptions import ExifValidationFailed, NoDepthMapFound, UnknownExtension
+from .face import find_bounding_box_teeth
 
 
-def load_image(fileName: str) -> Tuple[Image]:
+class IOSPortrait:
+    def __init__(
+        self,
+        photo,
+        depthmap=None,
+        teethmap=None,
+        floatValueMin=None,
+        floatValueMax=None,
+    ):
+        self.photo = photo
+        self.depthmap = depthmap
+        self.teethmap = teethmap
+
+        self.floatValueMin = floatValueMin
+        self.floatValueMax = floatValueMax
+
+        if self.floatValueMin is not None:
+            self.floatValueMin = float(self.floatValueMin)
+
+        if self.floatValueMax is not None:
+            self.floatValueMax = float(self.floatValueMax)
+
+        self.teeth_bbox = None
+        if self.teethmap:
+            self.teethmap = ImageOps.mirror(self.teethmap)
+            self.teethmap = self.teethmap.resize(self.photo.size)
+            ret = find_bounding_box_teeth(
+                self.teethmap.filter(ImageFilter.MaxFilter(size=13))
+            )
+            if ret is not None:
+                self.teeth_bbox = ret
+
+    def teeth_bbox_translated(self, max_wi, max_he):
+        if self.teeth_bbox is None:
+            return
+        x, y, wi, he = self.teeth_bbox
+        return (
+            x * max_wi / self.teethmap.size[0],
+            y * max_he / self.teethmap.size[1],
+            wi * max_wi / self.teethmap.size[0],
+            he * max_he / self.teethmap.size[1],
+        )
+
+
+def load_image(fileName: str) -> Union[IOSPortrait, None]:
     """
     Load HEIF or JPEG with depth data,
     return tuple of (image, depth)
@@ -33,8 +78,37 @@ def load_image(fileName: str) -> Tuple[Image]:
         if primary_image.depth_image is None:
             raise NoDepthMapFound(f"{fileName} has no depth data")
 
+        teeth_image = None
+        for looking_for in primary_image.auxiliary_images:
+            if (
+                getattr(looking_for, "type", "")
+                == "urn:com:apple:photo:2019:aux:semanticteethmatte"
+            ):
+                teeth_image = looking_for.image  # .load()
+                break
+
         depth_image = primary_image.depth_image.image.load()
-        depth_image = Image.frombytes(depth_image.mode, depth_image.size, depth_image.data)
+        ret = {}
+        if depth_image.metadata:
+            for metadata in depth_image.metadata:
+                if metadata.get("type", "") == "mime":
+                    import xml.etree.ElementTree as ET
+
+                    root = ET.fromstring(metadata.get("data"))
+                    for elem in root[0][0]:
+                        ret[elem.tag] = elem.text
+
+        float_min_value = ret.get(
+            "{http://ns.apple.com/pixeldatainfo/1.0/}FloatMinValue", 0.0
+        )
+
+        float_max_value = ret.get(
+            "{http://ns.apple.com/pixeldatainfo/1.0/}FloatMaxValue", 0.0
+        )
+
+        depth_image = Image.frombytes(
+            depth_image.mode, depth_image.size, depth_image.data
+        )
 
         picture_image = primary_image.image.load()
         try:
@@ -46,12 +120,41 @@ def load_image(fileName: str) -> Tuple[Image]:
             )
         except ValueError:
             # iPhone 12; probably flagged somewhere in the image; no idea, will just
-            # leave this as-is. 
+            # leave this as-is.
             picture_image = Image.frombytes(
                 picture_image.mode,
                 (picture_image.size[0], picture_image.size[1]),
                 picture_image.data,
             )
+        #     int aux_bit_depth = heif_image_handle_get_luma_bits_per_pixel(aux_handle);
+        #
+        #             struct heif_image* aux_image;
+        #             err = heif_decode_image(aux_handle,
+        #                                     &aux_image,
+        #                                     encoder->colorspace(false),
+        #                                     encoder->chroma(false, aux_bit_depth),
+        #                                     nullptr);
+
+        if teeth_image:
+            teeth_image: UndecodedHeifImage
+
+            teeth_image = teeth_image.load()
+            teeth_image = ImageOps.mirror(
+                Image.frombytes(
+                    "L",
+                    (teeth_image.size[0] * 3 + 14, teeth_image.size[1] - 1),
+                    teeth_image.data,
+                )
+            )
+            # teeth_image = teeth_image.scale(self.photo.size)
+
+        return IOSPortrait(
+            picture_image,
+            depth_image,
+            teeth_image,
+            float_min_value,
+            float_max_value,
+        )
 
     else:
         raise UnknownExtension(
